3c8c74b812c212b4a7b480ac917744547a76dc06
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-05-29 10:47:56 +0530

    Create seqcounter_tb.v

---
+++ Sequence_Counter/seqcounter_tb.v
@@ -0,0 +1,14 @@
+module seqcounter_tb(
+
+    );
+    reg clr,clk;
+    wire [3:0] q;
+    seqcounter s1(clr,clk,q);
+    initial
+    begin
+        clr=1'b0;
+        #10 clr=1'b1;clk=1'b0;
+        #60 $finish;
+    end
+    always #5 clk=~clk;
+endmodule
