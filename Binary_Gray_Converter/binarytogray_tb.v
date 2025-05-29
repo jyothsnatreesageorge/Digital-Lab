10f37984a68b5f952ded6e8551bedeb0b2c2fb87
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-05-29 11:06:52 +0530

    Create binarytogray_tb.v

---
+++ Binary_Gray_Converter/binarytogray_tb.v
@@ -0,0 +1,19 @@
+module binarytogray_tb(
+
+    );
+    reg [2:0] a;
+    wire [2:0] y;
+    binarytogray b1(a,y);
+    initial
+    begin
+        a=3'b000;
+        #10 a=3'b001;
+        #10 a=3'b010;
+        #10 a=3'b011;
+        #10 a=3'b100;
+        #10 a=3'b101;
+        #10 a=3'b110;
+        #10 a=3'b111;
+        #10 $finish;
+    end
+endmodule
